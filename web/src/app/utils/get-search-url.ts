6355e50a04bec6c441fb821bcc414b87cb9277de
lvsi <15239928381@163.com>
2025-06-15 02:27:16 +0800

    fix: 暂时去掉rid防止出现rid渲染问题

--- web/src/app/utils/get-search-url.ts
+++ web/src/app/utils/get-search-url.ts
@@ -2,5 +2,6 @@ export const getSearchUrl = (conversation_id: number = 0, query: string = "", mo
   // const prefix =
   //   process.env.NODE_ENV === "production" ? "/search.html" : "/search";
   const prefix = "/pages/search"
+  search_uuid = "0" // 先不要使用rid, 暂无使用场景
   return `${prefix}?q=${encodeURIComponent(query)}&conversation_id=${conversation_id}&mode=${mode}&rid=${search_uuid}`;
 };
